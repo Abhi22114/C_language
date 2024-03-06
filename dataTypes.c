805725062c4e65a172c9ed22927bd8b81db92585
Abhijeet Kumar <114794064+Abhi22114@users.noreply.github.com>
2024-03-07 02:11:14 +0530

    Create dataTypes.c

---
+++ dataTypes.c
@@ -0,0 +1,13 @@
+//Data Types
+//As explained in the Variables chapter, a variable in C must be a specified data type, and you must use a format specifier inside the printf() function to display it:
+
+//-->>Example
+// Create variables
+int myNum = 5;             // Integer (whole number)
+float myFloatNum = 5.99;   // Floating point number
+char myLetter = 'D';       // Character
+
+// Print variables
+printf("%d\n", myNum);
+printf("%f\n", myFloatNum);
+printf("%c\n", myLetter);
